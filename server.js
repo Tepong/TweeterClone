2f9a4c59e8b6380af177e230fc879d422353e94d
Tepong <te_pong@hotmail.com>
2015-12-07 16:33:03 +0700

    inital server file

---
+++ server.js
@@ -0,0 +1,19 @@
+// var connect = require('./node_modules/connect');
+// var app = connect();
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// var uri = 'mongodb://localhost/twitter_clone';
+
+// var db = mongoose.connect(config.mongodbUri);
+var mongoose = require('./config/mongoose');
+var express = require('./config/express');
+var passport = require('./config/passport');
+
+var db = mongoose();
+var app = express();
+var passport = passport();
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000');
+
+module.exports = app;
